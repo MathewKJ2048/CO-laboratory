190f0eefdb404ecd9270108b824eb48bca29b70e
Mathew K J <79681351+MathewKJ2048@users.noreply.github.com>
2022-02-28 17:22:55 +0530

    Delete Constants.java

--- Constants.java
+++
@@ -1,8 +0,0 @@
-
-
-public class Constants
-{
-    public static final String MULTI_LINE_START = "#*";
-    public static final String MULTI_LINE_END = "*#";
-    public static final String SINGLE_LINE = "#";
-}
