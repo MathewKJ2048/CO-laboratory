a294c6c3ac195312cc2ccf79e364f90fb972b95d
Mathew K J <79681351+MathewKJ2048@users.noreply.github.com>
2022-02-28 17:21:58 +0530

    Add files via upload

---
+++ compiler/Constants.java
@@ -0,0 +1,8 @@
+
+
+public class Constants
+{
+    public static final String MULTI_LINE_START = "#*";
+    public static final String MULTI_LINE_END = "*#";
+    public static final String SINGLE_LINE = "#";
+}
